a926047848dcff924d059de16462147b22dd5142
Julius Hsu <juliushsu07@gmail.com>
2022-11-30 15:36:02 -0500

    Finish and  add respnsive header styling.

--- main-site/src/components/header/CTA.jsx
+++ main-site/src/components/header/CTA.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import CV from '../../assets/cv.pdf'
 
 function CTA() {
   return (
     <>
     <div className='cta'>
-        <a href='{CV} download' className='btn'>Download CV</a>
+        <a href='#' className='btn'>My Resume</a>
         <a href='#contact' className='btn-primary'>Let's Talk</a>
     </div>
     </>
