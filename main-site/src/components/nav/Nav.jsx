3118428af579e4e59b628a9e10430222d90d849b
Julius Hsu <juliushsu07@gmail.com>
2022-12-01 12:31:44 -0500

    Create Nav bar and Update CSS for Header and Nav.

--- main-site/src/components/nav/Nav.jsx
+++ main-site/src/components/nav/Nav.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import './nav.css'
+import {BiHomeAlt} from 'react-icons/bi'
+import {RiUser5Line} from 'react-icons/ri'
+import {TbNotebook} from 'react-icons/tb'
+import {FaProjectDiagram} from 'react-icons/fa'
+import {AiOutlinePhone} from 'react-icons/ai'
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' className='active'><BiHomeAlt/></a>
+      <a href='#about'><RiUser5Line/></a>
+      <a href='#experience'><TbNotebook/></a>
+      <a href='#portfolio'><FaProjectDiagram/></a>
+      <a href='#contact'><AiOutlinePhone/></a>
+    </nav>
   )
 }
 
