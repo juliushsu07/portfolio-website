434387cc4c716d41e4a1d140952b078fbf119a92
Julius Hsu <juliushsu07@gmail.com>
2022-12-01 23:22:52 -0500

    Create skeleton css for Portfolio and Project.

--- main-site/src/components/portfolio/Portfolio.jsx
+++ main-site/src/components/portfolio/Portfolio.jsx
@@ -4,6 +4,9 @@ import Project from './Project'
 import IMG1 from '../../assets/portfolio1.jpg'
 import IMG2 from '../../assets/portfolio2.jpg'
 import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
 
 
 function Portfolio() {
@@ -28,6 +31,27 @@ function Portfolio() {
       "demoLink": "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
       "gitLink": "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
       "img" : IMG3
+    },
+    {
+      "id": 4,
+      "name": "Esmeralda Vanne",
+      "demoLink": "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+      "gitLink": "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+      "img" : IMG4
+    },
+    {
+      "id": 5,
+      "name": "Esmeralda Vanne",
+      "demoLink": "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+      "gitLink": "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+      "img" : IMG5
+    },
+    {
+      "id": 6,
+      "name": "Esmeralda Vanne",
+      "demoLink": "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+      "gitLink": "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+      "img" : IMG6
     },
   ]
 
--- main-site/src/components/portfolio/Project.jsx
+++ main-site/src/components/portfolio/Project.jsx
@@ -6,9 +6,11 @@ function Project({projectName, demoLink, gitLink, img}) {
         <div className='portfolio__item-image'>
             <img src ={img} alt=''/>
         </div>
+        <div className='project__buttons'>
         <h3>{projectName}</h3>
-        <a href={demoLink} className='btn' target='_blank'>GitHub</a>
-        <a href={gitLink} className='btn-primary' target='_blank'>Live Demo</a>
+          <a href={demoLink} className='btn' target='_blank'>GitHub</a>
+          <a href={gitLink} className='btn-primary' target='_blank'>Live Demo</a>
+        </div>
     </li>   
   )
 }
