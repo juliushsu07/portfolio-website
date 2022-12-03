ae5602151940b11c608eca7793784e8c41ffa5bb
Julius Hsu <juliushsu07@gmail.com>
2022-12-02 22:15:46 -0500

    Seperate Experiences and Experience and Install Vertical-Timeline Component to form each Experience dynamically in Experiences.

--- main-site/src/App.jsx
+++ main-site/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
-import Experience from './components/experience/Experience'
+import Experiences from './components/experience/Experiences'
 import Portfolio from './components/portfolio/Portfolio'
 import Contact from './components/contact/Contact'
 
@@ -14,7 +14,7 @@ const App = () => {
       <Header/>
       <Nav/>
       <About/>
-      <Experience/>
+      <Experiences/>
       <Portfolio/>
       <Contact/>
     </>
--- main-site/src/components/experience/Experience.jsx
+++ main-site/src/components/experience/Experience.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import {GrWorkshop} from 'react-icons/gr'
 
-function Experience() {
+function Experience({ date, jobTitle, location, achievements}) {
+  
+  const listItems = achievements.map((achievement, index) => 
+    <li key={index}>
+      - {achievement}
+    </li>
+  );
+
   return (
-    <section id='experience'></section>
+    <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        date={date}
+        iconStyle={{ background: "var(--color-white)" }}
+        icon={<GrWorkshop />}
+        >
+        <h3 className="vertical-timeline-element-title">{jobTitle}</h3>
+        <h4 className="vertical-timeline-element-subtitle">{location}</h4>
+        <ul className='achievements' >
+          {listItems}
+        </ul>
+    </VerticalTimelineElement>
   )
 }
 
