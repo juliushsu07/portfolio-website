f785875f397672a053a79bc5ee8b1efcb98e77e4
Julius Hsu <juliushsu07@gmail.com>
2022-12-05 17:38:23 -0500

    Add links to email, whatsapp msg api and emailjs api to Contact page.

--- main-site/src/components/contact/Contact.jsx
+++ main-site/src/components/contact/Contact.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 import './contact.css'
 import {HiOutlineMail} from 'react-icons/hi'
 import {AiOutlineWhatsApp} from 'react-icons/ai'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 function Contact() {
+  
+    const form = useRef();
+  
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_pjkr405', 'template_l3mt9b9', form.current, '1EJHdUabNu79klbBB')
+        .then((result) => {
+            alert('Email Sent!');
+            e.target.reset()
+        }, (error) => {
+            console.log(error.text);
+            alert('There was some ERROR!')
+        });        
+    };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -16,18 +34,18 @@ function Contact() {
             <HiOutlineMail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>juliushsu07@gmail.com</h5>
-            <a href='mailto:juliushsu07@gmail.com'>Send me a message</a>
+            <a href='mailto:juliushsu07@gmail.com' target='_blank'>Send a message</a>
           </article>
           
           <article className='contact__option'>
             <AiOutlineWhatsApp className='contact__option-icon'/>
             <h4>Phone Number</h4>
             <h5>+1 647 236 6568</h5>
-            <a href='mailto:juliushsu07@gmail.com'>Send me a message</a>
+            <a href='https://wa.me/16472366568' target='_blank'>Send a message</a>
           </article>
         
         </div>
-        <form action=''>
+        <form ref={form} onSubmit={sendEmail}>
           <input type='text' name='name' placeholder='Your Full Name' requred />
           <input type='email' name='email' placeholder='Your Email' required />
           <textarea name='message' row='7' placeholder='Your Message' required />
