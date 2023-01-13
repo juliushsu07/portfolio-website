4f3f61dc6f124b5ac0f18a5b563ccc61f6b14bf6
Julius Hsu <Juliushsu07@gmail.com>
2023-01-13 15:27:41 -0500

    Fix activeNav to display the actual current location Hash upon refresh.

--- main-site/src/App.jsx
+++ main-site/src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import { React, useState} from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
 import Experiences from './components/experience/Experiences'
-// import Portfolio from './components/portfolio/Portfolio'
+import Portfolio from './components/portfolio/Portfolio'
 import Contact from './components/contact/Contact'
 
-
-
 const App = () => {
+  const [activeNav, setActiveNav] = useState(window.location.hash ? window.location.hash : '#')
   return (
     <>
-      <Header/>
-      <Nav/>
-      <About/>
-      <Experiences/>
-      <Contact/>
+      <Nav activeNav={activeNav} setActiveNav={setActiveNav} />
+      <Header activeNav={activeNav} setActiveNav={setActiveNav} />
+      <About activeNav={activeNav} setActiveNav={setActiveNav} />
+      <Experiences activeNav={activeNav} setActiveNav={setActiveNav} />
+      <Portfolio activeNav={activeNav} setActiveNav={setActiveNav} />
+      <Contact activeNav={activeNav} setActiveNav={setActiveNav} />
     </>
   )
 }
--- main-site/src/components/nav/Nav.jsx
+++ main-site/src/components/nav/Nav.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
 import './nav.css'
-import {BiHomeAlt} from 'react-icons/bi'
-import {RiUser5Line} from 'react-icons/ri'
-import {TbNotebook} from 'react-icons/tb'
-import {FaProjectDiagram} from 'react-icons/fa'
-import {AiOutlinePhone} from 'react-icons/ai'
-import {useState} from 'react'
+import { BiHomeAlt } from 'react-icons/bi'
+import { RiUser5Line } from 'react-icons/ri'
+import { TbNotebook } from 'react-icons/tb'
+import { FaProjectDiagram } from 'react-icons/fa'
+import { AiOutlinePhone } from 'react-icons/ai'
 
-function Nav() {
-  const [activeNav, setActiveNav] = useState('#') 
+const Nav = (props) => {
+  const {activeNav, setActiveNav} = props
 
   return (
     <nav>
-      <a href='#' onClick={()=> setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><BiHomeAlt/></a>
-      <a href='#about' onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><RiUser5Line/></a>
-      <a href='#experience' onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><TbNotebook/></a>
-      {/* <a href='#portfolio' onClick={()=> setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><FaProjectDiagram/></a> */}
-      <a href='#contact' onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiOutlinePhone/></a>
+        <a href='#' onClick={()=> setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><BiHomeAlt/></a>
+        <a href='#about' onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><RiUser5Line/></a>
+        <a href='#experience' onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><TbNotebook/></a>
+        <a href='#portfolio' onClick={()=> setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><FaProjectDiagram/></a>
+        <a href='#contact' onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiOutlinePhone/></a>
     </nav>
   )
 }
